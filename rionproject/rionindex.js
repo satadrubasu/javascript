17cb4d6be2202fa99d3e12a316e5ddae57021045
satadrubasu <satadru.basu@gmail.com>
2018-10-22 17:33:25 +0530

    rion initial files saved to github

---
+++ rionproject/rionindex.js
@@ -0,0 +1,49 @@
+var ans1="'E-Mail'.";
+var ans2="'The scientific study of robot'.";
+var ans3="'Artificial Intelligence'.";
+var ans4="'It is a branch of computer science that aims to create intelligent machines that act like Humans'.";
+var ans5="'Expert System, Natural Language System, Perception System";
+var ans6="'It is the world’s of largest computer network, the ‘networks of networks’ scattered all over the world'.";
+var ans7="'To get information, to provide information and to compile information'.";
+var ans8="'To get information, to provide information and to compile information'.";
+var ans9="'To get information, to provide information and to compile information'.";
+var ans10="'To get information, to provide information and to compile information'.";
+
+
+function checkQuestion1() {
+    document.getElementById("answer1").value=ans1;
+}
+
+function checkQuestion2() {
+    document.getElementById("answer2").value=ans2;
+}
+        
+function checkQuestion3() {
+    document.getElementById("answer3").value=ans3;
+}
+
+function checkQuestion4() {
+    document.getElementById("answer4").value=ans4;
+}
+
+function checkQuestion5() {
+    document.getElementById("answer5").value=ans5;
+}
+
+function checkQuestion6() {
+    document.getElementById("answer6").value=ans6;
+}
+                                                   
+function checkQuestion7() {
+    document.getElementById("answer7").value=ans7;
+}
+
+function checkQuestion8() {
+    document.getElementById("answer8").value=ans8;
+}
+function checkQuestion9() {
+    document.getElementById("answer9").value=ans9;
+}
+function checkQuestion10() {
+    document.getElementById("answer10").value=ans10;
+}
